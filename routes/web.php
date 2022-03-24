25770a12a67abf7d0f2b91ae98f82db6890bf2b7
MuhamadSholikhudin <muhammadsholihudin18@gmail.com>
2022-03-25 05:26:38 +0700

    mempelajari laravel

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\HiEmployee;
 use App\Http\Controllers\HiEmployeeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SearchController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,3 +34,19 @@ Route::get('/hi', function () {
 Route::resource('/hi/employees', HiEmployeeController::class);
 
 Route::resource('/hi/investigations', HiEmployeeController::class);
+
+
+//EXCEL IMPORT END EXPORT
+Route::controller(UserController::class)->group(function(){
+    Route::get('users', 'index');
+    Route::get('users-export', 'export')->name('users.export');
+    Route::post('users-import', 'import')->name('users.import');
+});
+
+//SELECT2 
+Route::controller(SearchController::class)->group(function(){
+    Route::get('demo-search', 'index');
+    Route::get('autocomplete', 'autocomplete')->name('autocomplete');
+});
+
+
